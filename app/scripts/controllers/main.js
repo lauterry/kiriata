1d8ab748976d4e91c9abc18803b07a928ab5e0e1
LAU Thierry <lau.t@sfeir.com>
2012-09-18 21:41:35 +0200

    First Commit Use Yeoman with AngularJs Generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+kiriataApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
---
+++ app/scripts/kiriata.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+var kiriataApp = angular.module('kiriataApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
